8de0d93be12aed7a7c982117fb63ed2af0de55d6
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-31 19:03:28 +0900

    [Update]ユーザー削除とチーム編集を制御するため修正

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    if current_user != @team.owner
+      render :show
+    end
+  end
 
   def create
     @team = Team.new(team_params)
